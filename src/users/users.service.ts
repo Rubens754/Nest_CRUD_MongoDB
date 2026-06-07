113c12ded9cb0172ba63df16dc2ef7151fa621b6
Rubens Melo <rubensmelosantos@gmail.com>
2026-06-07 11:57:55 -0300

    Primeira versão do CRUD NestJS com MongoDB Atlas

---
+++ src/users/users.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Model } from 'mongoose';
+
+import {
+  User,
+  UserDocument,
+} from './schemas/user.schema';
+
+@Injectable()
+export class UsersService {
+
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<UserDocument>,
+  ) {}
+
+  create(data: any) {
+    return this.userModel.create(data);
+  }
+
+  findAll() {
+    return this.userModel.find();
+  }
+
+  findOne(id: string) {
+    return this.userModel.findById(id);
+  }
+
+  update(id: string, data: any) {
+    return this.userModel.findByIdAndUpdate(
+      id,
+      data,
+      { new: true },
+    );
+  }
+
+  remove(id: string) {
+    return this.userModel.findByIdAndDelete(id);
+  }
+}
